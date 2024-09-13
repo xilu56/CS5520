8fd4f37b55c52e64f44ae5aece0049f937af95bc
xilu56 <xilu56@gmail.com>
2024-09-12 20:18:01 -0700

    Refactor Input component to include useRef and useEffect hooks

--- Components/Input.js
+++ Components/Input.js
@@ -1,24 +1,29 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import {useState} from 'react';
+import { useRef} from 'react';
+import { useEffect } from 'react';
 import { TextInput } from 'react-native';
 
 export default function Input({shouldFocus}) {
     const [text, setText] = useState("");
+    const textInputRef = useRef(null);
 
-    // function to update the text
-    function updateText(ChangeText){
-      setText(ChangeText);
+  useEffect(() => {
+    if(shouldFocus && textInputRef.current){
+      textInputRef.current.focus();
     }
+  }, [shouldFocus]); 
+
   return (
     <View>
-      <TextInput 
+      <TextInput
+        ref={textInputRef} 
         placeholder='Type something' 
         keyboardType='default' 
         style={{borderBottomColor: "purple", borderBottomWidth: 2}}
         value={text}
         onChangeText= {updateText}
-        autoFocus={shouldFocus} //set autoFocus based on the prop
         />
     </View>
   )
