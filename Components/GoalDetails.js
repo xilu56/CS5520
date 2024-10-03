a67cdbdac4833193379df82bbabb4710613c08cf
xilu56 <xilu56@gmail.com>
2024-10-03 12:00:22 -0700

    Added more details button pushes Details screen on to the stack

--- Components/GoalDetails.js
+++ Components/GoalDetails.js
@@ -1,17 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-
 export default function GoalDetails({ navigation, route }) {
-  console.log(route.params.goalData);
+  console.log(route);
+  function moreDetailsHandler() {
+    navigation.push("Details");
+  }
+
   return (
     <View>
-      <Text>
-        This is details of a goal with text {route.params.goalData.text} and id{" "}
-        {route.params.goalData.id}
-      </Text>
+      {route.params ? (
+        <Text>
+          This is details of a goal with text {route.params.goalData.text} and
+          id {route.params.goalData.id}
+        </Text>
+      ) : (
+        <Text>More details</Text>
+      )}
+      <Button title="More Details" onPress={moreDetailsHandler} />
     </View>
   );
 }
-
 const styles = StyleSheet.create({});
