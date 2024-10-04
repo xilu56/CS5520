eea41635e78f5549460a7bd87d4bcee1cec932bd
xilu56 <xilu56@gmail.com>
2024-10-03 21:36:26 -0700

    navigate directly from GoalItem.js using useNavigation hook rather than callback

--- Components/GoalItem.js
+++ Components/GoalItem.js
@@ -1,21 +1,19 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
-export default function GoalItem({ goalObj, deleteHandler, pressHandler }) {
+export default function GoalItem({ goalObj, deleteHandler}) {
   function handleDelete() {
     console.log("deleted");
     deleteHandler(goalObj.id);
   }
-  function handlePress() {
-    // call a callbackfn received from parent
-    //pass the goal obj back to Home.js
-    pressHandler(goalObj);
-  }
+  const navigation = useNavigation();
   return (
     <View style={styles.textContainer}>
       <Text style={styles.text}>{goalObj.text}</Text>
       <Button title="X" color="grey" onPress={handleDelete} />
-      <Button title="i" color="grey" onPress={handlePress} />
+      <Button title="i" color="grey" onPress={function () 
+        {navigation.navigate("Details",{ goalData: goalObj }); }}/>
     </View>
   );
 }
