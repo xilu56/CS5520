12f40aa8b46bde513d1b59d64ec3683383bb7bdf
xilu56 <xilu56@gmail.com>
2024-10-10 11:25:46 -0700

    Removed the i button and added a pressable in GoalItem

--- Components/GoalItem.js
+++ Components/GoalItem.js
@@ -1,23 +1,29 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 
-export default function GoalItem({ goalObj, deleteHandler}) {
+export default function GoalItem({ goalObj, deleteHandler }) {
+  const navigation = useNavigation();
   function handleDelete() {
     console.log("deleted");
     deleteHandler(goalObj.id);
   }
-  const navigation = useNavigation();
+  function handlePress() {
+    // call a callbackfn received from parent
+    //pass the goal obj back to Home.js
+    // pressHandler(goalObj);
+    navigation.navigate("Details", { goalData: goalObj });
+  }
   return (
     <View style={styles.textContainer}>
-      <Text style={styles.text}>{goalObj.text}</Text>
-      <Button title="X" color="grey" onPress={handleDelete} />
-      <Button title="i" color="grey" onPress={function () 
-        {navigation.navigate("Details",{ goalData: goalObj }); }}/>
+      <Pressable onPress={handlePress} style={styles.horizontalContainer}>
+        <Text style={styles.text}>{goalObj.text}</Text>
+        <Button title="X" color="grey" onPress={handleDelete} />
+        {/* <Button title="i" color="grey" onPress={handlePress} /> */}
+      </Pressable>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   text: {
     color: "purple",
@@ -28,6 +34,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#aaa",
     borderRadius: 5,
     marginVertical: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  horizontalContainer: {
     flexDirection: "row",
     alignItems: "center",
   },
