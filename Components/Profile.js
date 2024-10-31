0b15b795743d95f04dfe9cedafa2103af63f33f5
xilu56 <xilu56@gmail.com>
2024-10-31 13:17:00 -0700

    Refactor Profile component to remove unnecessary code and display user email and UID

--- Components/Profile.js
+++ Components/Profile.js
@@ -1,100 +1,14 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet, Alert } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 import { auth } from "../Firebase/firebaseSetup";
 
-export default function Signup({ navigation }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-
-  const loginHandler = () => {
-    //take user to login
-    navigation.replace("Login");
-  };
-  const signupHandler = async () => {
-    // do some validation:
-    //1. email, password and confirmpassword are not empty
-    //2.
-    //create a new user using createUserWithEmailAndPassword
-    try {
-      if (
-        email.length === 0 ||
-        password.length === 0 ||
-        confirmPassword.length === 0
-      ) {
-        Alert.alert("All fields should be provided");
-        return;
-      }
-      if (password !== confirmPassword) {
-        Alert.alert("password and confirm password don't match");
-        return;
-      }
-      // any other check you could do to make sure we have valid data
-      //e.g. regex for email, password length,...
-      const userCred = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      console.log(userCred.user);
-    } catch (err) {
-      console.log("sign up ", err);
-      Alert.alert(err.message);
-    }
-  };
+export default function Profile() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Email</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={(changedText) => {
-          setEmail(changedText);
-        }}
-      />
-      <Text style={styles.label}>Password</Text>
-      <TextInput
-        style={styles.input}
-        secureTextEntry={true}
-        placeholder="Password"
-        value={password}
-        onChangeText={(changedText) => {
-          setPassword(changedText);
-        }}
-      />
-      <Text style={styles.label}>Confirm Password</Text>
-      <TextInput
-        style={styles.input}
-        secureTextEntry={true}
-        placeholder="Confirm Password"
-        value={confirmPassword}
-        onChangeText={(changedText) => {
-          setConfirmPassword(changedText);
-        }}
-      />
-      <Button title="Register" onPress={signupHandler} />
-      <Button title="Already Registered? Login" onPress={loginHandler} />
+    <View>
+      <Text>{auth.currentUser.email}</Text>
+      <Text>{auth.currentUser.uid}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    // alignItems: "stretch",
-    justifyContent: "center",
-  },
-  input: {
-    borderColor: "#552055",
-    borderWidth: 2,
-    width: "90%",
-    margin: 5,
-    padding: 5,
-  },
-  label: {
-    marginLeft: 10,
-  },
-});
+const styles = StyleSheet.create({});
